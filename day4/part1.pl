1ff867db0134a9c7496f22348e366fe01fe8a19a
Marco Sirabella <marco@sirabella.org>
2019-12-05 00:31:22 -0500

    Sloppy commit with both answers & time taken

---
+++ day4/part1.pl
@@ -0,0 +1,27 @@
+%!/usr/bin/env swipl
+:- use_module(library(clpfd)).
+
+% near(A, B) :- A #= B + 1; A #= B - 1.
+% adjacent_doubles(L) :- element(IA, L, A), element(IB, L, A), near(IA, IB).
+adjacent_doubles([A, A|_]).
+adjacent_doubles([_|R]) :- adjacent_doubles(R).
+
+incrementing([_]).
+incrementing([A,B|R]) :- A #=< B, incrementing([B|R]), !.
+
+password(N, [A, B, C, D, E, F]) :- F #= N mod 10, E #= N // 10 mod 10, D #= N // 100 mod 10, C #= N // 1000 mod 10, B #= N // 10000 mod 10, A #= N // 100000 mod 10.
+
+within_range(L, R) :- N in R, password(N, L).
+
+valid_password(L) :- within_range(L, 272091..815432), adjacent_doubles(L), incrementing(L).
+
+part1(N) :- setof(L, (valid_password(L), label(L)), C), length(C, N).
+
+
+test([B, A, A]) :- B #\= A.
+test([B, A, A, C|_]) :- B #\= A, C #\= A.
+test([B|R]) :- test(R).
+
+shy_adjacent_doubles(R) :- test([-1|R]).
+
+part2(N) :- setof(L, (valid_password(L), shy_adjacent_doubles(L), label(L)), C), length(C, N).
