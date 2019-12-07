a65333530cb321b065cee7dd220c7cfd50264447
Marco Sirabella <marco@sirabella.org>
2019-12-07 11:12:53 -0500

    d7p1 complete w/ answer
    I gotta go back and improve the previous days

---
+++ day7/max.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function permut() {
+	python3 <<-EOF
+	import itertools
+	inp = [int(l) for l in '$@'.split(' ')]
+	print("\n".join(" ".join(str(c) for c in l) for l in itertools.permutations(inp)))
+	EOF
+}
+
+function phase() {
+	(echo $1; cat) | ./a.out
+}
+
+function amplify() {
+	echo 0 | phase $1 | phase $2 | phase $3 | phase $4 | phase $5
+}
+
+
+permut 0 1 2 3 4 | while read line; do
+	echo $line $(amplify $line)
+done | sort -n -k 6
