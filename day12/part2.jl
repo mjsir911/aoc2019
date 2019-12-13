e7c4791881ff5c2d50e3615de4b63f7b0af7cb64
Marco Sirabella <marco@sirabella.org>
2019-12-12 21:15:23 -0500

    Optimize

--- day12/part2.jl
+++ day12/part2.jl
@@ -1,5 +1,10 @@
 include("part1.jl")
 
+function gravity(me::Body{1}, others::Vector{Body{1}})::Body{1}
+	vel = me.vel[1] + sum(sign(o.pos[1] - me.pos[1]) for o in others)
+	return Body{1}((me.pos[1] + vel,), (vel,))
+end
+
 function period(bs::Vector{Body{N}}) where N
 	for (i, newbs) in enumerate(simulate(bs))
 		if newbs == bs
