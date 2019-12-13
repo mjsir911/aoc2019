46d097aa8c032b17149d78e9822e0bb8f5a53a14
Marco Sirabella <marco@sirabella.org>
2019-12-12 20:37:13 -0500

    Use fixed length arrays for 4x slowdown

--- day12/part1.jl
+++ day12/part1.jl
@@ -1,13 +1,15 @@
-struct Body
-		 pos::Vector{Int}
-		 vel::Vector{Int}
+import StaticArrays
+
+struct Body{DimN}
+		pos::StaticArrays.SVector{DimN,Int}
+		vel::StaticArrays.SVector{DimN,Int}
 end
 
-function newvel(me::Body, other::Body)
+function newvel(me::Body{N}, other::Body{N}) where N
 	return sign.(other.pos - me.pos)
 end
 
-function interact(me::Body, others::Vector{Body})
+function interact(me::Body, others::Vector{Body{N}})::Body{N} where N
 	vel = me.vel
 	for b in others
 		if b == me
@@ -22,14 +24,13 @@ function energy(me::Body)
 	return sum(abs, me.pos) * sum(abs, me.vel)
 end
 
-function Body(p::Vector{Int})
-	n = length(p)
-	return Body(p, zeros(n))
+function Body(p::NTuple{N, Int}) where N
+	return Body{N}(p, zeros(N))
 end
-b1 = Body([-13, 14, -7])
-b2 = Body([-18, 9, 0])
-b3 = Body([0, -3, -3])
-b4 = Body([-15, 3, -13])
+b1 = Body((-13, 14, -7))
+b2 = Body((-18, 9, 0))
+b3 = Body((0, -3, -3))
+b4 = Body((-15, 3, -13))
 
 
 bodies = [b1, b2, b3, b4]
