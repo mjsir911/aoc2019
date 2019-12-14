8aec47b9c4e174ed91156f29f563354de95de820
Marco Sirabella <marco@sirabella.org>
2019-12-14 18:50:12 -0500

    Add input parsing

--- day14/test.py
+++ day14/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # vim: set fileencoding=utf-8 :
 from itertools import repeat
+from sys import stdin
 
 
 class Factory():
@@ -9,25 +10,48 @@ class Factory():
         self.inputs = inputs
         self.amount_left = 0
 
+    def __iter__(self):
+        return self
+
     def __next__(self):
         self.amount_left -= 1
         if self.amount_left > 0:
             return 0
         else:
             self.amount_left = self.max_buf
-            return sum(next(i) for i in self.inputs)
+            return sum(next(zip(*self.inputs)))
+
+    @classmethod
+    def fromGraph(cls, g, goal, known=None):
+        if known is None:
+            known = {}
+
+        if goal not in known:
+            # this is a mess
+            # see V for at least an explanation for the double comprehension
+            # https://spapas.github.io/2016/04/27/python-nested-list-comprehensions/
+            ins = [a for subgoal, amount in g[goal][1].items() for a in [cls.fromGraph(g, subgoal, known)] * amount]
+            known[goal] = cls(g[goal][0], ins)
+        return known[goal]
 
 
 ore = repeat(1)
 
-a = Factory(2, [ore] * 9)
-b = Factory(3, [ore] * 8)
-c = Factory(5, [ore] * 7)
+dependency_graph = {}
 
-ab = Factory(1, [a] * 3 + [b] * 4)
-bc = Factory(1, [b] * 5 + [c] * 7)
-ca = Factory(1, [c] * 4 + [a] * 1)
 
-fuel = Factory(1, [ab] * 2 + [bc] * 3 + [ca] * 4)
+def parse(s):
+    amount, name = s.split(' ')
+    return name, int(amount)
 
+
+for line in stdin:
+    ins, out = line.strip().split(' => ')
+    ins = dict(parse(i) for i in ins.split(', '))
+    out = parse(out)
+
+    dependency_graph[out[0]] = (out[1], ins)
+
+# print(dependency_graph)
+fuel = Factory.fromGraph(dependency_graph, 'FUEL', {'ORE': ore})
 print(next(fuel))
